66ee653408f26037710e989b658ae3437699e95d
Frankva <24254885+Frankva@users.noreply.github.com>
2024-12-19 16:28:23 +0100

    refactor(backend): in progress to add role

---
+++ backend/src/main/java/ch/sectioninformatique/template/security/PermissionRepository.java
@@ -0,0 +1,11 @@
+package ch.sectioninformatique.template.security;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PermissionRepository extends CrudRepository<Permission, Integer> {
+    Optional<Role> findByName(RoleEnum name);
+}
